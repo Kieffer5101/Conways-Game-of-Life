ed60dae247b71f253a145ae0a8187bc16f341f9e
Liam Frank <liam.frank@wsu.edu>
2025-04-15 15:40:08 -0700

    neighbor checker mostly complete + print to the screen.

--- main.cpp
+++ main.cpp
@@ -4,12 +4,12 @@
 //
 
 #include <cstdlib>
+#include <cmath>
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
 #include "configuration.h"
 #include "events.h"
-
 
 
 
@@ -21,56 +21,165 @@ int main()
     // charcoal gray
     sf::Color grey = {54, 69, 79};
 
+    //int boarderOffset = conf::board_size.x+3;
+    sf::Vector2u cellLookup = {0, 0};
+    int generation = 0;
+    int neighbors = 0;
+    int top_right,
+        bottom_right,
+        bottom_left,
+        top_left;
+
     // seed the random number generator
     std::srand(std::time(0));
+    
 
     /// Build the grid
 
-    std::vector<sf::RectangleShape> boardGrid(conf::cell_count);
-    std::cout << "Built a sf::rectangleShape vector with a length of : " << boardGrid.size() << std::endl;
+    std::vector<std::vector<int>> lifeGrid(conf::board_size.y + 2, std::vector<int>(conf::board_size.x + 2, 0));
+    std::vector<std::vector<int>> lifeGrid_B(conf::board_size.y + 2, std::vector<int>(conf::board_size.x + 2, 0));
 
-    sf::Vector2f location = {0.f, 0.f};
+    std::vector<sf::RectangleShape> visualGrid(conf::cell_count);
+    std::cout << "Built a sf::rectangleShape vector with a length of : " << visualGrid.size() << std::endl;
+
+    sf::Vector2f cellLocation = {0.f, 0.f};
     for (int i {}; i < conf::cell_count; i++) {
 
         if (i % (conf::board_size.x) == 0 && i > 0) {
-            location += {0, conf::window_size_f.y / conf::board_size.y};
-            location.x = 0;
+            cellLocation += {0, conf::window_size_f.y / conf::board_size.y};
+            cellLocation.x = 0;
             //std::cout << "newline detected at position [" << i << "]\n";
         }
-        boardGrid[i].setSize({conf::window_size_f.x / conf::board_size.x, conf::window_size_f.y / conf::board_size.y});
-        boardGrid[i].setPosition(location);
-        boardGrid[i].setOutlineColor(grey);
-        boardGrid[i].setOutlineThickness(-1.f);
+        visualGrid[i].setSize({conf::window_size_f.x / conf::board_size.x, conf::window_size_f.y / conf::board_size.y});
+        visualGrid[i].setPosition(cellLocation);
+        visualGrid[i].setOutlineColor(grey);
+        visualGrid[i].setOutlineThickness(-1.f);
 
         // randomly determine the life or death of a cell
-        if (std::rand() % 2 == 1) 
-            boardGrid[i].setFillColor(sf::Color::White);
-        else
-            boardGrid[i].setFillColor(sf::Color::Black);
 
+        if (std::rand() % 2 == 1) {
+            lifeGrid[1 + std::floor(i / conf::board_size.x)][1 + (i % conf::board_size.x)] = 1;
+            visualGrid[i].setFillColor(sf::Color::White);
+        }
+        else {
+            lifeGrid[1 + std::floor(i / conf::board_size.x)][1 + (i % conf::board_size.x)] = 0;
+            visualGrid[i].setFillColor(sf::Color::Black);
+        }
 
-        //std::cout << i << " built cell :\nLocation : " << boardGrid[i].getPosition().x << ", " << boardGrid[i].getPosition().y << "\n";
+        //std::cout << i << " built cell :\nLocation : " << visualGrid[i].getPosition().x << ", " << visualGrid[i].getPosition().y << "\n";
 
-        location += {conf::window_size_f.x / conf::board_size.x, 0};
+        cellLocation += {conf::window_size_f.x / conf::board_size.x, 0};
     }
-    sf::Vector2f size = boardGrid[0].getSize();
+    sf::Vector2f size = visualGrid[0].getSize();
 
-    boardGrid[24].setFillColor(sf::Color::White);
+    int row = 0;
+    int collumn = 0;
+
+    for (unsigned int i {}; i < conf::cell_count; i++) { // print board loop
+        cellLookup = {1 + std::floor(i / conf::board_size.x), 1 + (i % conf::board_size.x)};
+
+        if (i % conf::board_size.x == 0) {
+            std::cout << "\n";
+            row++;
+        }
+        if (lifeGrid[cellLookup.x][cellLookup.y] == 0) {
+            std::cout << ". ";
+            collumn++;
+        }
+        else {
+            std::cout << "# ";
+            collumn++;
+        }
+    }
+    std::cout << std::endl;
+    std::cout << "Rows : " << row << "\nCollumns : " << collumn/row << std::endl;
     
 
     // main program loop
     while (window.isOpen())
     {
-        processEvents(window, boardGrid);
-
-        window.clear();
+        processEvents(window, visualGrid);
 
         // display the grid
+        window.clear();
         for (int i {}; i < conf::cell_count; i++) {
 
-            window.draw(boardGrid[i]);
+            window.draw(visualGrid[i]);
         }
-
         window.display();
+
+        // update the cells 
+        
+        for (int i {}; i < conf::cell_count; i++) {
+            cellLookup = {1 + std::floor(i / conf::board_size.x), 1 + (i % conf::board_size.x)};
+
+            top_left     = lifeGrid[cellLookup.x - 1][cellLookup.y - 1];
+            bottom_left  = lifeGrid[cellLookup.x - 1][cellLookup.y + 1];
+            top_right    = lifeGrid[cellLookup.x + 1][cellLookup.y - 1];
+            bottom_right = lifeGrid[cellLookup.x + 1][cellLookup.y + 1];
+            neighbors = 0;
+
+            if (!(i % conf::board_size.x == 0)) { // check if possition is NOT on the left wall
+                // check the 3 neighbors to the left 
+                neighbors += 
+                      lifeGrid[i - 1 - conf::board_size.x] 
+                    + lifeGrid[i - 1] 
+                    + lifeGrid[i - 1 + conf::board_size.x]
+                    - top_left;
+            }
+
+            if (!((i - conf::board_size.x) < 0)) { // check if position is NOT on the top wall
+                // check the 3 neighbors above
+                neighbors += 
+                      lifeGrid[i - conf::board_size.x - 1] 
+                    + lifeGrid[i - conf::board_size.x]
+                    + lifeGrid[i - conf::board_size.x + 1] 
+                    - top_right;
+
+            }
+
+            if (!((i + 1) % conf::board_size.x == 0)) { // check if position is NOT on the right wall
+                // check the 3 neighbors to the right
+                neighbors += 
+                    lifeGrid[i + 1 - conf::board_size.x]) 
+                    + checkValidity(lifeGrid[i + 1]) 
+                    + checkValidity(lifeGrid[i + 1 + conf::board_size.x])) 
+                    - bottom_right;
+            }
+
+            if (!(i - (conf::board_size.x * conf::board_size.y - conf::board_size.x) >= 0)) { // check if position is NOT on the bottom wall
+                // check the 3 neighbors below
+                neighbors += (checkValidity(lifeGrid[i + conf::board_size.x - 1]) 
+                    + checkValidity(lifeGrid[i + conf::board_size.x]) 
+                    + checkValidity(lifeGrid[i + conf::board_size.x + 1])) 
+                    - bottom_left;
+            }
+            //std::cout << "board[" << i << "] : " << neighbors << "\n";
+
+            switch (neighbors) {
+
+            case 1: {
+                lifeGrid_B[i] = 0;
+                visualGrid[i].setFillColor(sf::Color::Black);
+                break;
+            }
+            case 2:
+                if (lifeGrid[i] == 0)
+                    break;
+            case 3: {
+                lifeGrid_B[i] = 1;
+                visualGrid[i].setFillColor(sf::Color::White);
+                break;
+            }
+            default:
+                lifeGrid_B[i] = 0;
+                visualGrid[i].setFillColor(sf::Color::Black);
+            }
+        }
+        std::copy(lifeGrid_B, lifeGrid_B + (conf::cell_count), lifeGrid);
+
+        generation++;
+        */
+
     }
-}
+    }
